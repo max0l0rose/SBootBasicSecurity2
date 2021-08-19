5385a1b18cfb29822993dfc0d66bb20d40d8d3c0
maxanarki <qqq12345>
2021-08-19 21:43:56 +0300

    @ModelAttribute ErrorController, AbstractErrorController
    public void displayHeaderInfo(@CookieValue("JSESSIONID") String cookie)  {
    }
    
    @ModelAttribute() doesn't work:
    
     @GetMapping("error")
        @ExceptionHandler(Exception.class)
        public String error(@ModelAttribute("exception") Exception exception
        ) {
        }
    
            @ModelAttribute("s")
            String gets() {
                return "qqq";
            }
    works in @Controller only. @ControllerAdvice - not

--- src/main/java/rc/bootsecurity/controller/MyErrorController.java
+++ src/main/java/rc/bootsecurity/controller/MyErrorController.java
@@ -1,6 +1,11 @@
 package rc.bootsecurity.controller;
 
 
+import javassist.NotFoundException;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
@@ -8,16 +13,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
-public class MyErrorController implements ErrorController
+public class MyErrorController
+
+        //Marker interface used to identify a @Controller that should be used to render errors.
+        //implements ErrorController
+
+        extends AbstractErrorController
 {
+
+    public MyErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+
+
+    @ModelAttribute("s")
+    String gets() {
+        return "qqq";
+    }
+
+
 
     @GetMapping("error")
     //@ExceptionHandler(Exception.class)
@@ -26,8 +51,10 @@ public class MyErrorController implements ErrorController
             // Exception ex
             //, Model model
             //, RedirectAttributes ra
+            @ModelAttribute("s") String s,
             HttpServletRequest request
-    ) {
+                                        ) throws NotFoundException
+    {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         Object msg = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
         Object uri = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
@@ -38,6 +65,9 @@ public class MyErrorController implements ErrorController
             Integer code = Integer.valueOf(status.toString());
             if (code == 403)
                 throw new AccessDeniedException("qqqqqqqqqqqqqqqq");
+            else
+            if (code == 404)
+                throw new NotFoundException("wwwwwww");
 //
 //            if(statusCode == HttpStatus.NOT_FOUND.value()) {
 ////                return "error-404";
--- src/main/java/rc/bootsecurity/controller/MyExceptionHandler.java
+++ src/main/java/rc/bootsecurity/controller/MyExceptionHandler.java
@@ -1,27 +1,35 @@
 package rc.bootsecurity.controller;
 
 import javassist.NotFoundException;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class MyExceptionHandler
-        extends ResponseEntityExceptionHandler
+        //extends ResponseEntityExceptionHandler
 {
 
     @GetMapping("error")
     @ExceptionHandler(Exception.class)
     public String error(//HttpServletRequest request
                         //@RequestHeader(name = "errorcode") String errorCode
+                        //ErrorAttributes errorAttributes
+                        Model model,
+                        @ModelAttribute("exception") Exception exception
     ) {
         //String a = errorCode;
+        //model.addAttribute("exception", exception);
         return "error";
     }
 
@@ -34,5 +42,14 @@ public class MyExceptionHandler
         return new ResponseEntity<Object>(
                 "Access denied message here", new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
+
+
+//    @ExceptionHandler({ NotFoundException.class })
+//    public ResponseEntity<Object> handlenfe(
+//            Exception ex, WebRequest request)
+//    {
+//        return new ResponseEntity<Object>(
+//                "nfe", new HttpHeaders(), HttpStatus.NOT_FOUND);
+//    }
 
 }
